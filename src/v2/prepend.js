a4cf71f829f7813365500ef79ac6a3c2aa8e2c96
ben.greer <ben.greer@service-now.com>
2017-03-06 08:31:27 -0800

    v2: weekend progress on prepend operation

---
+++ src/v2/prepend.js
@@ -0,0 +1,125 @@
+import {one, fromFocusOf} from './_constructors';
+import {
+	focusOnFirstBlockǃ,
+	makeTransientIfNeeded,
+	makeTransientSizes,
+	setupNewBlockInInitBranch} from './_vectorPointer';
+import {arraycopy} from './_array';
+
+/**
+ *
+ * @param {T} value
+ * @param {Node<T>} rrb
+ * @return {Node<T>}
+ */
+export function prepend(value, list) {
+
+	if (list.endIndex === 0) {
+		return one(value);
+	}
+
+	var x = fromFocusOf(list);
+	x.endIndex = list.endIndex;
+	x.transient = list.transient;
+	x.endIndex = list.endIndex + 1
+
+	focusOnFirstBlockǃ(x)
+	if (list.display0.length < 32) { // element fits in current block
+		prependOnCurrentBlock(value, x)
+	} else {
+		prependFrontNewBlock(value, x)
+	}
+
+	return x
+}
+
+
+function prependOnCurrentBlock(value, list) {
+	var oldD0 = list.display0;
+	var newLen = oldD0.length + 1
+	list.focusEnd = newLen
+
+	var d0 = new Array(newLen);
+	d0[0] = value;
+	for (var i = 0, l = oldD0.length; l > i; i++) {
+		d0[i + 1] = oldD0[i];
+	}
+
+	list.display0 = d0;
+	makeTransientIfNeeded(list)
+}
+
+
+function prependFrontNewBlock(elem, list) {
+
+var currentDepth = list.focusDepth
+	if (currentDepth == 1)
+		currentDepth += 1
+
+	if (currentDepth == 1)
+		currentDepth = 2;
+	var display = list['display' + (currentDepth - 1)]
+
+	while (display != null && display.length == 33) { /* the insertion depth has not been found */
+		currentDepth += 1
+		switch (currentDepth) {
+			case 2 : display = list.display1; break;
+			case 3 : display = list.display2; break;
+			case 4 : display = list.display3; break;
+			case 5 : display = list.display4; break;
+			case 6 : display = list.display5; break;
+		}
+	}
+
+	var oldDepth = list.depth
+
+	// create new node at this depth and all singleton nodes under it on left most branch
+	setupNewBlockInInitBranch(currentDepth, list.transient, list)
+
+	// update sizes of nodes above the insertion depth
+	if (oldDepth == list.depth) { // setupNewBlockInNextBranch(...) increased the depth of the tree
+		var i = currentDepth
+		if (i < oldDepth) {
+			var _focusDepth = list.focusDepth
+			display = list['display' + i]
+			do {
+				var displayLen = display.length - 1
+				var newSizes = i >= _focusDepth ? makeTransientSizes(display[displayLen], 1) : null
+
+				var newDisplay = new Array(display.length)
+				arraycopy(display, 1, newDisplay, 1, displayLen - 1)
+				if (i >= _focusDepth)
+					newDisplay[displayLen] = newSizes
+
+				switch (i) {
+					case 2:
+						list.display2 = newDisplay
+						display = list.display3;
+						break;
+					case 3:
+						list.display3 = newDisplay
+						display = list.display4;
+						break;
+					case 4:
+						list.display4 = newDisplay
+						display = list.display5
+						break;
+					case 5:
+						list.display5 = newDisplay
+						break;
+				}
+				i += 1
+			} while (i < oldDepth)
+		}
+	}
+
+	list.focus = 0;
+	list.focusStart = 0;
+	list.focusEnd = 1;
+	list.focusDepth = 1;
+	list.focusRelax = 0;
+	list.display0[0] = elem;
+	list.transient = true
+	return list;
+}
+
--- test/pure.test.js
+++ test/pure.test.js
@@ -1,58 +1,90 @@
-import {empty, one} from '../src/pure/_constructors';
-import {append} from '../src/pure/append';
-// import {appendǃ} from '../src/pure/_append';
-import nth from '../src/pure/nth';
+import {empty, one} from '../src/v2/_constructors';
+import {append} from '../src/v2/append';
+import {prepend} from '../src/v2/prepend';
+import nth from '../src/v2/nth';
 import {expect} from 'chai';
 
 describe("rrb with focus tests", function() {
-	var none;
-	var uno;
+	var depths = [32, 1024, 32768, 1048576,33554432, 1073741824]
 
-	it('constructor tests', function() {
-		none = empty();
-		uno = one(10);
+	describe("basic construction tests", function() {
+		var none;
+		var uno;
+
+		it('constructor tests', function() {
+			none = empty();
+			uno = one(10);
+		});
+
+		it('append 32 test', function() {
+			var vec = empty();
+			for (var i = 0; 32 > i; i++) {
+				vec = append(i, vec);
+			}
+		});
+
+		it('append 10 000 test', function() {
+			var vec = empty();
+			for (var i = 0; 10000 > i; i++) {
+				vec = append(i, vec);
+			}
+		});
+
+		it('append 100 000 test', function() {
+			var vec = empty();
+			for (var i = 0; 100000 > i; i++) {
+				vec = append(i, vec);
+			}
+		});
+
+		it('append 1 000 000 test', function() {
+			var vec = empty();
+			for (var i = 0; 1000000 > i; i++) {
+				vec = append(i, vec);
+			}
+		});
+
+		it.skip('append 1 000 000 native test', function() {
+			var vec = [];
+			for (var i = 0; 1000000 > i; i++) {
+				vec.push(i);
+			}
+		});
 	});
 
-	it('append 32 test', function() {
-		var vec = empty();
-		for (var i = 0; 32 > i; i++) {
-			vec = append(i, vec);
-		}
-	});
+	describe('prepend tests', function() {
+		var pvec = empty();
+		var MAX = 10000;
 
-	it('append 10 000 test', function() {
-		var vec = empty();
-		for (var i = 0; 10000 > i; i++) {
-			vec = append(i, vec);
-		}
-	});
+		it('prepend 1 000 000 test', function() {
+			//*
+			for (var i = 0; MAX > i; i++) {
+				pvec = prepend(i, pvec);
+			}
+			/*/
+			 try {
+				 for (var i = 0; MAX > i; i++) {
+					 pvec = prepend(i, pvec);
+				 }
+			 } catch (e) {
+				 throw new Error(JSON.stringify(pvec))
+			 }
+			//*/
 
-	it('append 100 000 test', function() {
-		var vec = empty();
-		for (var i = 0; 100000 > i; i++) {
-			vec = append(i, vec);
-		}
-	});
+		});
 
-	it('append 1 000 000 test', function() {
-		var vec = empty();
-		for (var i = 0; 1000000 > i; i++) {
-			vec = append(i, vec);
-		}
-	});
-
-	it.skip('append 1 000 000 native test', function() {
-		var vec = [];
-		for (var i = 0; 1000000 > i; i++) {
-			vec.push(i);
-		}
-	});
+		it('prepend 1 000 000 ordering test', function() {
+			for (var i = MAX; i--;) {
+				expect(nth(i, pvec, 'missing')).to.equal(i);
+			}
+		});
+	})
 
 	describe('ordered get/set confirmation', function() {
-		var depths = [32, 1024, 32768, 1048576,33554432, 1073741824]
 
-		it('retrieves 1000 items in same order as inserted', function() {
-			var MAX = 1000
+
+		it('retrieves 10000 items in same order as inserted', function() {
+			var MAX = 10000
 			var vec = empty();
 
 			for (var i = 0; MAX > i; i++) {
